fc5c0551dabf6defb1a68361b33178cef18580da
Jonny-Burkholder <jbburkholder@gmail.com>
2022-08-09 11:18:30 -0400

    update logger

---
+++ pkg/arbiter/logger.go
@@ -0,0 +1,30 @@
+package arbiter
+
+import "fmt"
+
+// Logger is a simple system for logging errors
+// and other such things
+type Logger struct {
+	Route string `json:"route"` //route defines where the logger writes to
+}
+
+func (l *Logger) Write(v ...any) error {
+	if l.Route == "stdout" || l.Route == "std.Out" || l.Route == "" {
+		fmt.Println(v...)
+		return
+	}
+	// TODO: write v to log
+	return nil
+}
+
+func (l *Logger) Writef(s string, v ...any) error {
+
+	var towrite string
+
+	if len(v) > 0 {
+		towrite = fmt.Sprintf(s, v...)
+	}
+	// TODO: write string to log
+
+	return nil
+}
